f975520f647b3cf9b960f65344e455a472ca85f6
eugene195 <eugene.07@list.ru>
2016-08-28 12:32:48 -0400

    Added raw input preprocessing
    Space squash and trim applied to the raw input

--- inputprocessor.cpp
+++ inputprocessor.cpp
@@ -2,26 +2,22 @@
 #include "userinput.h"
 #include "mapexecutabletooptions.h"
 #include "executecommand.h"
+#include <algorithm>
 
-void InputProcessor::process(string rawCommand)
+void InputProcessor::process(string& rawCommand)
 {
-    UserInput command = rawCommand;
-    for (AbstractProcessingStep *step : steps) {
-        command = step->processCommand(command);
+    rawCommand = squashSpaces(trim(rawCommand));
+    if (!rawCommand.empty()) {
+        UserInput command = rawCommand;
+        for (AbstractProcessingStep *step : steps) {
+            command = step->processCommand(command);
+        }
     }
 }
 
 InputProcessor::InputProcessor()
 {
-/**
- * FIXME
-*/
-    AbstractProcessingStep *tokenize = new TokenizeStep;
-    AbstractProcessingStep *mapToOptions = new MapExecutableToOptions;
-    AbstractProcessingStep *executeCommand = new ExecuteCommand;
-    this->steps.push_back(tokenize);
-    this->steps.push_back(mapToOptions);
-    this->steps.push_back(executeCommand);
+    this->steps = {new TokenizeStep, new MapExecutableToOptions, new ExecuteCommand};
 }
 
 InputProcessor::~InputProcessor()
@@ -29,5 +25,28 @@ InputProcessor::~InputProcessor()
     for (auto* step : steps) {
         delete step;
     }
+}
+
+string &InputProcessor::trimRight(string &s, const string &delimiters)
+{
+    return s.erase(s.find_last_not_of(delimiters) + 1);
+}
+
+string &InputProcessor::trimLeft(string &s, const string &delimiters)
+{
+    return s.erase(0, s.find_first_not_of(delimiters));
+}
+
+string &InputProcessor::trim(string &s, const string &delimiters)
+{
+    return trimLeft(trimRight(s, delimiters), delimiters);
+}
+
+string& InputProcessor::squashSpaces(string &s)
+{
+    s.erase(unique(s.begin(), s.end(),
+                   [](char a, char b) { return isspace(a) && isspace(b); }),
+            s.end());
+    return s;
 }
 
--- inputprocessor.h
+++ inputprocessor.h
@@ -12,12 +12,16 @@ using namespace std;
 class InputProcessor
 {
 public:
-    void process (string rawCommand);
+    void process (string &rawCommand);
     InputProcessor();
     ~InputProcessor();
 
 private:
     vector<AbstractProcessingStep *> steps;
+    string& trimRight(string& s, const string& delimiters = " \f\n\r\t\v");
+    string& trimLeft(string& s, const string& delimiters = " \f\n\r\t\v");
+    string& trim(string& s, const string& delimiters = " \f\n\r\t\v");
+    string &squashSpaces(string& s);
 };
 
 #endif // INPUTPROCESSOR_H
